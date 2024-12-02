dd81c9604e3839cf20d92bbc07114e5e6f3d6233
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-12-02 21:11:56 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,27 @@ int main(int argc, char* argv[]) {
         std::cout << "未指定文件路径" << std::endl;
     }
     char const* file_path = argv[1];
+
     // 1. 分配上下文空间
     AVFormatContext* format_context = avformat_alloc_context();
+
     // 2. 打开视频/音频流文件
     avformat_open_input(&format_context, file_path, nullptr, nullptr);
+
     // 3. 获取流信息
+    // - format_context->iformat->name
+    // - format_context->duration
+    // - format_context->bit_rate
     avformat_find_stream_info(format_context, nullptr);
 
-    // NOTE: 这里比特率并不等于视频编解码器比特率+音频编解码器比特率
-    printf("封装器格式 %s, 时长 %ld s, 比特率 %ld bit/s\n", format_context->iformat->name,
-           (format_context->duration) / 1000000, format_context->bit_rate);
+    // 4. 循环获取视频/音频流
+    // AVCodecParameters* codec_params = nullptr;  // 被编码流的各种属性
+    // AVCodec* codec = nullptr;                   // 编解码器信息
+
+    for (int i{0}; i < format_context->nb_streams; ++i) {
+        // 获取编解码器参数
+        AVCodecParameters* codec_params = format_context->streams[i]->codecpar;
+        // 根据编解码器类型 ID (AV_CODEC_ID_H264, AV_CODEC_ID_HEVC...) 查找解码器
+        AVCodec const* codec = avcodec_find_decoder(codec_params->codec_id);
+    }
 }
