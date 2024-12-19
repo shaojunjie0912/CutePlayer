b74503bdd9e46acf325d402261b9da1fe02b3e4c
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-12-19 23:09:30 +0800

    update

--- src/simple_audio_player/test_audio.cpp
+++ src/simple_audio_player/test_audio.cpp
@@ -1,51 +1,65 @@
-// #include <cstdint>
-// #include <cstring>
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 extern "C" {
 #include <SDL2/SDL.h>
 };
 
-// struct Sound {
-//     uint8_t* data;
-//     uint32_t len;
-//     uint32_t pos = 0;
-// };
+struct Sound {
+    uint8_t* data;
+    uint32_t len;
+    uint32_t pos = 0;
+};
 
-// void AudioCallback(void* userdata, uint8_t* stream, int len) {
-//     Sound* sound = (Sound*)userdata;
-//     memset(stream, 0, len);
-//     int size = (len > sound->len - sound->pos) ? sound->len - sound->pos : len;
-//     memcpy(stream, sound->data + sound->pos, size);
-//     sound->pos += size;
-// }
+void AudioCallback(void* userdata, uint8_t* stream, int len) {
+    Sound* sound = (Sound*)userdata;
+    memset(stream, 0, len);
+    int size = (len > sound->len - sound->pos) ? sound->len - sound->pos : len;
+    memcpy(stream, sound->data + sound->pos, size);
+    sound->pos += size;
+}
 
-int main() {
+int main(int argc, char* argv[]) {
     char const* audio_file = nullptr;
     int ret = SDL_Init(SDL_INIT_EVERYTHING);
 
-    SDL_Window* window =
-        SDL_CreateWindow("SDL Video Player", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300,
-                         300, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
-    //     if (ret != 0) {
-    //         cout << "Could not initialize SDL - " << SDL_GetError() << endl;
-    //         return -1;
-    //     }
-    // #ifdef WAV_FILE
-    //     audio_file = WAV_FILE;
-    // #else
-    //     cout << "没有指定音频文件" << endl;
-    // #endif
-    //     SDL_AudioSpec wav_spec;
-    //     Sound sound;
-    //     if (!SDL_LoadWAV(audio_file, &wav_spec, &sound.data, &sound.len)) {
-    //         SDL_Log("无法加载音频文件 %s - %s", audio_file, SDL_GetError());
-    //     }
+    if (ret != 0) {
+        cout << "Could not initialize SDL - " << SDL_GetError() << endl;
+        return -1;
+    }
+#ifdef WAV_FILE
+    audio_file = WAV_FILE;
+#else
+    cout << "没有指定音频文件" << endl;
+#endif
+    SDL_AudioSpec wav_spec;
+    Sound sound;
+    if (!SDL_LoadWAV(audio_file, &wav_spec, &sound.data, &sound.len)) {
+        SDL_Log("无法加载音频文件 %s - %s", audio_file, SDL_GetError());
+    }
 
-    //     wav_spec.userdata = &sound;
-    //     wav_spec.callback = AudioCallback;
-    // SDL_OpenAudio(&wav_spec, nullptr);
-    // SDL_PauseAudio(0);
-    SDL_DestroyWindow(window);
+    wav_spec.userdata = &sound;
+    wav_spec.callback = AudioCallback;
+    SDL_OpenAudio(&wav_spec, nullptr);
+    SDL_PauseAudio(0);
+    while (true) {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT: {
+                SDL_Quit();
+                exit(0);
+            } break;
+
+            default: {
+                // nothing to do
+            } break;
+        }
+    }
     SDL_Quit();
     return 0;
 }
